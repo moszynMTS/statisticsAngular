a58891f69b0ddb15b13a426bd24889a2f9d3f6d3
Szymon <szmsmi@gmail.com>
2024-12-02 15:53:05 +0100

    diagram automation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren, AfterViewInit, ElementRef } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
@@ -8,11 +8,8 @@ import { environment } from 'src/environments/environment';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  @ViewChild('barCanvas', { static: true }) barCanvas!: ElementRef<HTMLCanvasElement>;
-  @ViewChild('pieCanvas', { static: true }) pieCanvas!: ElementRef<HTMLCanvasElement>;
-  @ViewChild('lineCanvas', { static: true }) lineCanvas!: ElementRef<HTMLCanvasElement>;
-  @ViewChild('doughnutCanvas', { static: true }) doughnutCanvas!: ElementRef<HTMLCanvasElement>;
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChildren('canvas') canvases!: QueryList<ElementRef<HTMLCanvasElement>>;
   usedDataSet: string = environment.dataSet;
   chartColors: string[] = [
     '#E6F4EA',
@@ -26,183 +23,166 @@ export class HomeComponent implements OnInit {
     '#1F4F23',
     '#133719',
   ];
-  barForm!: FormGroup;
-  pieForm!: FormGroup;
-  lineForm!: FormGroup;
-  doughnutForm!: FormGroup;
-  charts=[this.barForm, this.pieForm, this.lineForm, this.doughnutForm];
-
+  charts: any[] = [];
+  chartTypes: any[] = [
+    { title: "Słupkowy", type: 'bar', description: 'Chart słupkowy (Bar chart) przedstawia dane w postaci prostokątnych słupków o różnej wysokości.' },
+    { title: "Liniowy", type: 'line', description: 'Wykres liniowy (Line chart) pokazuje dane jako serię punktów połączonych liniami, co pozwala na zobaczenie trendów.' },
+    { title: "Kołowy", type: 'pie', description: 'Wykres kołowy (Pie chart) pokazuje proporcje między kategoriami w postaci wycinków koła.' },
+    { title: "Donutowy", type: 'doughnut', description: 'Wykres donutowy (Doughnut chart) jest podobny do wykresu kołowego, ale ma pusty środek, co pozwala na lepsze wizualizowanie danych.' },
+  ];
+  
   constructor(private fb: FormBuilder) {
     Chart.register(...registerables);
   }
 
   ngOnInit(): void {
     this.initializeForms();
+  }
+
+  ngAfterViewInit(): void {
     this.createCharts();
   }
 
-  //#region Initialize Forms
   initializeForms(): void {
-    this.barForm = this.fb.group({
-      canvas: ["barCanvas", Validators.required],
-      name: ["#1 Bar", Validators.required],
-      title: ["Bar", Validators.required],
-      data: [[12, 19, 3, 5, 2], Validators.required],
-      labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
-      showedConfig: [false, Validators.required],
-    });
-
-    this.pieForm = this.fb.group({
-      canvas: ["pieCanvas", Validators.required],
-      name: ["#2 Pie", Validators.required],
-      title: ["Pie", Validators.required],
-      data: [[12, 19, 3, 5, 2], Validators.required],
-      labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
-    });
-
-    this.lineForm = this.fb.group({
-      canvas: ["lineCanvas", Validators.required],
-      name: ["#3 Line", Validators.required],
-      title: ["Line", Validators.required],
-      data: [[10, 15, 5, 7, 10], Validators.required],
-      labels: [['January', 'February', 'March', 'April', 'May'], Validators.required],
-    });
-
-    this.doughnutForm = this.fb.group({
-      canvas: ["barCanvas", Validators.required],
-      name: ["#4 Doughnut", Validators.required],
-      title: ["Doughnut", Validators.required],
-      data: [[12, 19, 3, 5, 2], Validators.required],
-      labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
-    });
+    let counter = 1;
+    this.charts = [
+      { form: this.fb.group({
+          canvas: [`canvas-${counter}`],
+          type: ['bar', Validators.required],
+          name: [`#${counter++} Bar`, Validators.required],
+          title: ["Bar", Validators.required],
+          data: [[12, 19, 3, 5, 2], Validators.required],
+          labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
+          showedConfig: [false, Validators.required],
+        })
+      },
+      { form: this.fb.group({
+          canvas: [`canvas-${counter}`],
+          type: ['bar', Validators.required],
+          name: [`#${counter++} Bar`, Validators.required],
+          title: ["Bar", Validators.required],
+          data: [[12, 19, 3, 5, 2], Validators.required],
+          labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
+          showedConfig: [false, Validators.required],
+        })
+      },
+      { form: this.fb.group({
+          canvas: [`canvas-${counter}`],
+          type: ['line', Validators.required],
+          name: [`#${counter++} Line`, Validators.required],
+          title: ["Line", Validators.required],
+          data: [[10, 15, 5, 7, 10], Validators.required],
+          labels: [['January', 'February', 'March', 'April', 'May'], Validators.required],
+          showedConfig: [false, Validators.required],
+        })
+      },
+      { form: this.fb.group({
+          canvas: [`canvas-${counter}`],
+          type: ['pie', Validators.required],
+          name: [`#${counter++} Pie`, Validators.required],
+          title: ["Pie", Validators.required],
+          data: [[12, 19, 3, 5, 2], Validators.required],
+          labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
+          showedConfig: [false, Validators.required],
+        })
+      },
+      { form: this.fb.group({
+          canvas: [`canvas-${counter}`],
+          type: ['doughnut', Validators.required],
+          name: [`#${counter++} Doughnut`, Validators.required],
+          title: ["Doughnut", Validators.required],
+          data: [[12, 19, 3, 5, 2], Validators.required],
+          labels: [['Red', 'Blue', 'Yellow', 'Green', 'Orange'], Validators.required],
+          showedConfig: [false, Validators.required],
+        })
+      },
+    ];
   }
-  //#endregion
+  
 
   switchState(form: FormGroup): void {
     const currentState = form.value.showedConfig;
     form.patchValue({
-      showedConfig: !currentState
+      showedConfig: !currentState,
     });
   }
 
-  //#region Create Charts
   createCharts(): void {
-    this.createBarChart();
-    this.createLineChart();
-    this.createPieChart();
-    this.createDoughnutChart();
-  }
-  //#endregion
+    this.canvases.toArray().forEach((canvasElement, index) => {
+      const chart = this.charts[index];
+      const data = chart.form.value.data;
+      const labels = chart.form.value.labels;
+      const title = chart.form.value.title;
+      const type = chart.form.value.type;
 
-  //#region Bar
-  createBarChart(): void {
-    const data = this.barForm.value.data;
-    const labels = this.barForm.value.labels;
-    const title = this.barForm.value.title;
-
-    new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels,
-        datasets: [
-          {
-            label: title,
-            data,
-            backgroundColor: this.chartColors,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: { position: 'top' },
+      new Chart(canvasElement.nativeElement, {
+        type: type,
+        data: {
+          labels,
+          datasets: [
+            {
+              label: title,
+              data,
+              backgroundColor: this.chartColors,
+              borderColor: this.chartColors[1],
+              borderWidth: 2,
+              fill: false,
+            },
+          ],
         },
-      },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: { position: 'top' },
+          },
+        },
+      });
     });
   }
-  //#endregion
+  reloadCanvas(chart: any): void {
+    const canvasElement = this.canvases.toArray()[this.charts.indexOf(chart)];
+    if (canvasElement && canvasElement.nativeElement) {
+      const existingChart = Chart.getChart(canvasElement.nativeElement);
+      if (existingChart) {
+        existingChart.destroy();
+      }
+      let data = this.parseArray(chart.form.value.data);
+      let labels = this.parseArray(chart.form.value.labels);
+      let title = this.parseArray(chart.form.value.title); //todo based on type
+      const type = chart.form.value.type;
 
-  //#region Line
-  createLineChart(): void {
-    const data = this.lineForm.value.data;
-    const labels = this.lineForm.value.labels;
-    const title = this.doughnutForm.value.title;
-
-    new Chart(this.lineCanvas.nativeElement, {
-      type: 'line',
-      data: {
-        labels,
-        datasets: [
-          {
-            label: title,
-            data,
-            borderColor: this.chartColors[1],
-            borderWidth: 2,
-            fill: false,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: { position: 'top' },
+      new Chart(canvasElement.nativeElement, {
+        type: type,
+        data: {
+          labels,
+          datasets: [
+            {
+              label: title,
+              data,
+              backgroundColor: this.chartColors,
+              borderColor: this.chartColors[1],
+              borderWidth: 2,
+              fill: false,
+            },
+          ],
         },
-      },
-    });
-  }
-  //#endregion
-
-  //#region Pie
-  createPieChart(): void {
-    const data = this.pieForm.value.data;
-    const labels = this.pieForm.value.labels;
-    const title = this.pieForm.value.title;
-
-    new Chart(this.pieCanvas.nativeElement, {
-      type: 'pie',
-      data: {
-        labels,
-        datasets: [
-          {
-            data,
-            backgroundColor: this.chartColors,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: { position: 'top' },
           },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: { position: 'bottom' },
         },
-      },
-    });
+      });
+    }
   }
-  //#endregion
-
-  //#region Doughnut
-  createDoughnutChart(): void {
-    const data = this.doughnutForm.value.data;
-    const labels = this.doughnutForm.value.labels;
-    const title = this.doughnutForm.value.title;
-
-    new Chart(this.doughnutCanvas.nativeElement, {
-      type: 'doughnut',
-      data: {
-        labels,
-        datasets: [
-          {
-            data,
-            backgroundColor: this.chartColors,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: { position: 'bottom' },
-        },
-      },
-    });
+  parseArray(input: any): any[] {
+    if (typeof input === 'string') {
+      return input.split(',').map((item: string) => item.trim());
+    }
+    return Array.isArray(input) ? input : [];
   }
-  //#endregion
+  
+  addNext(){
+    console.log("AddNewItemPlaceholder")
+  }
 }
