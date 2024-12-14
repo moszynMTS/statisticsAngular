00398844f15ba590557a48e157ec6354ee86527f
Szymon <szmsmi@gmail.com>
2024-12-14 15:26:38 +0100

    translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from 'src/environments/environment';
 
@@ -7,10 +8,24 @@ import { environment } from 'src/environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private translate: TranslateService) {
+export class AppComponent implements OnInit {
+  constructor(
+    private translate: TranslateService,
+    private titleService: Title
+  ) {
     this.translate.setDefaultLang('pl');
     this.translate.use(environment.lang);
   }
-  title = 'StatisticsAngular';
+
+  ngOnInit(): void {
+    this.setTitle();
+    this.translate.onLangChange.subscribe(() => {
+      this.setTitle();
+    });
+  }
+
+  private setTitle(): void {
+    const translatedTitle = this.translate.instant('App.Name');
+    this.titleService.setTitle(translatedTitle);
+  }
 }
